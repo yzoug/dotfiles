419c9c9aa4fd3d156479989c5198b99993b24134
yzoug <zoug@protonmail.ch>
2024-10-31 22:46:26 +0100

    Small misc config changes

--- dot_config/rofi/wifi/executable_wifi.sh
+++ dot_config/rofi/wifi/executable_wifi.sh
@@ -10,4 +10,4 @@ theme='style-1'
 bssid=$(nmcli device wifi list | sed -n '1!P'| cut -b 9- | rofi -dmenu -theme ${dir}/${theme}.rasi -p " " -lines 10 | awk '{print $1}')
 [ -z "$bssid" ] && exit 1
 nmcli device wifi connect $bssid
-notify-send "󱚽  WiFi connected\n${bssid}"
+notify-send "󱚽  WiFi connected"
