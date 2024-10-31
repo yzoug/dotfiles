11c97157d03ff501425f8d579b33d9d7f0e32462
yzoug <zoug@protonmail.ch>
2024-10-31 22:43:17 +0100

    Ignore swayidle when home

--- bin/executable_lockscreen.sh
+++ bin/executable_lockscreen.sh
@@ -1,18 +1,29 @@
 #!/usr/bin/env bash
 # Lockscreen to use with Sway, one or many monitors, imagemagick, jq and Swaylock
-# Will screenshot the laptop monitor and only that monitor, then scales down the image
-# The image is scaled back up: pixels have been lost which gives a blur effect
+# Screenshots the specified monitor then scales down the image then back up
+# Pixels have been lost which gives a blur effect
+#
+# The script does nothing if invoked by swayidle and a specified hardware monitor is connected: I don't need lockscreen at home.
+#
 # zoug
 #
 
-LAPTOP_MONITOR_NAME="eDP-1"
+# Don't apply lockscreen if invoked by swayidle and I'm at home
+# Enter a string identifier for your monitor
+HOME_MONITOR_IDENTIFIER="PXMGC2ADAFLL"
+if swaymsg -t get_outputs | grep ${HOME_MONITOR_IDENTIFIER} && [[ $(ps -o comm= $PPID) == "swayidle" ]]; then
+    notify-send -u low "You're at home!" "Use keybinding to lock manually."
+    exit 0
+fi
 
 # Temporary files to screenshot and resized images
 TEMP_BASE=$(mktemp -t lockscreen_img_XXXXXXXX.png)
 TEMP_SMALL=$(mktemp -t lockscreen_img_XXXXXXXX.png)
 TEMP_FINAL=$(mktemp -t lockscreen_img_XXXXXXXX.png)
 
-# Get first monitor (laptop monitor) mode
+# Get laptop monitor mode and position
+# These values don't change but this way the script is portable
+LAPTOP_MONITOR_NAME="eDP-1"
 MODE_LAPTOP_MONITOR=$(swaymsg -t get_outputs | jq -r ".[] | select(.name==\"${LAPTOP_MONITOR_NAME}\").current_mode | \"\(.width)x\(.height)\"")
 POSITION_LAPTOP_MONITOR=$(swaymsg -t get_outputs | jq -r ".[] | select(.name==\"${LAPTOP_MONITOR_NAME}\").rect | \"\(.x),\(.y)\"")
 
