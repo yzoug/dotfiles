8ae49d7f20ef368dfaf464e718aa51070106be08
Yassine Zouggari <git@zoug.fr>
2025-03-11 15:11:36 +0100

    Improve screenshot.sh script
    Add a debug flag and improve fullscreen screenshot: now only screenshots
    the focused monitor

--- bin/executable_screenshot.sh
+++ bin/executable_screenshot.sh
@@ -4,12 +4,20 @@
 #
 
 # input: SCREENSHOT_MODE variable
-# full: for screenshot of fullscreen
+# "full": for fullscreen screenshot of focused monitor
 # any other value: default (ask region from slurp)
 
-FILENAME_SCREENSHOT=~/shots/$(date +%F_%T).png
+[[ ${DEBUG} -eq 1 ]] && set -x
+
+FILENAME_SCREENSHOT=~/shots/$(date +%F-%T).png
 if [[ ${SCREENSHOT_MODE} == "full" ]]; then
-    grim -g - - | tee >(wl-copy) > ${FILENAME_SCREENSHOT} && notify-send -i ${FILENAME_SCREENSHOT} "Screenshot of full desktop taken" -t 1000
+    # Get focused monitor mode and position
+    # These values don't change but this way the script is portable
+    FOCUSED_MONITOR_NAME=$(swaymsg -t get_outputs | jq -r ".[] | select(.focused) | .name")
+    MODE_LAPTOP_MONITOR=$(swaymsg -t get_outputs | jq -r ".[] | select(.name==\"${FOCUSED_MONITOR_NAME}\").current_mode | \"\(.width)x\(.height)\"")
+    POSITION_LAPTOP_MONITOR=$(swaymsg -t get_outputs | jq -r ".[] | select(.name==\"${FOCUSED_MONITOR_NAME}\").rect | \"\(.x),\(.y)\"")
+
+    echo -n "${POSITION_LAPTOP_MONITOR} ${MODE_LAPTOP_MONITOR}" | grim -g - - | tee >(wl-copy) > ${FILENAME_SCREENSHOT} && notify-send -i ${FILENAME_SCREENSHOT} "Screenshot of full desktop taken" -t 1000
 else
     slurp | grim -g - - | tee >(wl-copy) > ${FILENAME_SCREENSHOT} && notify-send -i ${FILENAME_SCREENSHOT} "Screenshot of region taken" -t 1000
 fi
