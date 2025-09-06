60c1d26c1125c1990cf79ec3d8c434969faf5214
Yassine Zouggari <git@zoug.fr>
2025-09-06 13:35:06 +0200

    Update screenshot.sh and lockscreen.sh
    Use new screen identifier (ignored in my setup by the lockscreen.sh
    script) and simplify lockscreen.sh to rely on screenshot.sh.
    screenshot.sh is also improved to add a mode to not copy the screenshot
    to the clipboard (in this case, for use with the lockscreen) and to
    allow specifying a custom screenshot path for the saved file. Other
    small improvements also done (shellcheck, factorizations...)

--- bin/executable_lockscreen.sh
+++ bin/executable_lockscreen.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-# Lockscreen to use with Sway, one or many monitors, imagemagick, jq and Swaylock
-# Screenshots the specified monitor then scales down the image then back up
+# Lockscreen to use with Sway, imagemagick, jq and Swaylock
+# Screenshot using my screenshot.sh script, scales down the image then back up
 # Pixels have been lost which gives a blur effect
 #
 # The script does nothing if invoked by swayidle and a specified hardware monitor is connected: I don't need lockscreen at home.
@@ -10,7 +10,7 @@
 
 # Don't apply lockscreen if invoked by swayidle and I'm at home
 # Enter a string identifier for your monitor
-HOME_MONITOR_IDENTIFIER="PXMGC2ADAFLL"
+HOME_MONITOR_IDENTIFIER="HNMY105546"
 if swaymsg -t get_outputs | grep ${HOME_MONITOR_IDENTIFIER} && [[ $(ps -o comm= $PPID) == "swayidle" ]]; then
     notify-send -u low "You're at home!" "Use keybinding to lock manually."
     exit 0
@@ -21,22 +21,16 @@ TEMP_BASE=$(mktemp -t lockscreen_img_XXXXXXXX.png)
 TEMP_SMALL=$(mktemp -t lockscreen_img_XXXXXXXX.png)
 TEMP_FINAL=$(mktemp -t lockscreen_img_XXXXXXXX.png)
 
-# Get laptop monitor mode and position
-# These values don't change but this way the script is portable
-LAPTOP_MONITOR_NAME="eDP-1"
-MODE_LAPTOP_MONITOR=$(swaymsg -t get_outputs | jq -r ".[] | select(.name==\"${LAPTOP_MONITOR_NAME}\").current_mode | \"\(.width)x\(.height)\"")
-POSITION_LAPTOP_MONITOR=$(swaymsg -t get_outputs | jq -r ".[] | select(.name==\"${LAPTOP_MONITOR_NAME}\").rect | \"\(.x),\(.y)\"")
-
 # Take screenshot
-grim -g "${POSITION_LAPTOP_MONITOR} ${MODE_LAPTOP_MONITOR}" ${TEMP_BASE}
+SCREENSHOT_FILENAME="${TEMP_BASE}" SCREENSHOT_MODE="no-clip" screenshot.sh
 
 # Shrink it down
-magick convert -resize 10% ${TEMP_BASE} ${TEMP_SMALL}
+magick convert -resize 10% "${TEMP_BASE}" "${TEMP_SMALL}"
 # Then back up
-magick convert -resize 1000% ${TEMP_SMALL} ${TEMP_FINAL}
+magick convert -resize 1000% "${TEMP_SMALL}" "${TEMP_FINAL}"
 
 # Apply lockscreen
-swaylock -f -i ${TEMP_FINAL}
+swaylock -f -i "${TEMP_FINAL}"
 
 # Cleanup
-rm ${TEMP_SMALL} ${TEMP_BASE} ${TEMP_FINAL}
+rm "${TEMP_SMALL}" "${TEMP_BASE}" "${TEMP_FINAL}"
--- bin/executable_screenshot.sh
+++ bin/executable_screenshot.sh
@@ -1,23 +1,29 @@
 #!/usr/bin/env bash
-# Simple script to save screenshots in clipboard + in dedicated folder
+# Simple script to save screenshots in clipboard and a file
+# For use with Sway, slurp, grim, wl-copy and jq
 # zoug
 #
 
 # input: SCREENSHOT_MODE variable
-# "full": for fullscreen screenshot of focused monitor
-# any other value: default (ask region from slurp)
+# "full": for fullscreen screenshot of focused monitor and copy in clipboard
+# "no-clip": for fullscreen screenshot without copying to clipboard
+# any other value: ask region from slurp and copy in clipboard
 
 [[ ${DEBUG} -eq 1 ]] && set -x
 
-FILENAME_SCREENSHOT=~/shots/$(date +%F-%T).png
-if [[ ${SCREENSHOT_MODE} == "full" ]]; then
-    # Get focused monitor mode and position
-    # These values don't change but this way the script is portable
-    FOCUSED_MONITOR_NAME=$(swaymsg -t get_outputs | jq -r ".[] | select(.focused) | .name")
-    MODE_LAPTOP_MONITOR=$(swaymsg -t get_outputs | jq -r ".[] | select(.name==\"${FOCUSED_MONITOR_NAME}\").current_mode | \"\(.width)x\(.height)\"")
-    POSITION_LAPTOP_MONITOR=$(swaymsg -t get_outputs | jq -r ".[] | select(.name==\"${FOCUSED_MONITOR_NAME}\").rect | \"\(.x),\(.y)\"")
+# Where the screenshot is saved
+# Either supply this env variable to the script, or it goes to my default folder
+SCREENSHOT_FILENAME="${SCREENSHOT_FILENAME:=$HOME/shots/$(date +%F-%T).png}"
 
-    echo -n "${POSITION_LAPTOP_MONITOR} ${MODE_LAPTOP_MONITOR}" | grim -g - - | tee >(wl-copy) > ${FILENAME_SCREENSHOT} && notify-send -i ${FILENAME_SCREENSHOT} "Screenshot of full desktop taken" -t 1000
+# Get focused monitor name, mode and position
+FOCUSED_MONITOR_NAME=$(swaymsg -t get_outputs | jq -r ".[] | select(.focused) | .name")
+FOCUSED_MONITOR_MODE=$(swaymsg -t get_outputs | jq -r ".[] | select(.name==\"${FOCUSED_MONITOR_NAME}\").current_mode | \"\(.width)x\(.height)\"")
+FOCUSED_MONITOR_POSITION=$(swaymsg -t get_outputs | jq -r ".[] | select(.name==\"${FOCUSED_MONITOR_NAME}\").rect | \"\(.x),\(.y)\"")
+
+if [[ ${SCREENSHOT_MODE} == "full" ]]; then
+    echo -n "${FOCUSED_MONITOR_POSITION} ${FOCUSED_MONITOR_MODE}" | grim -g - - | tee >(wl-copy) > "${SCREENSHOT_FILENAME}" && notify-send -i "${SCREENSHOT_FILENAME}" "Screenshot of full desktop taken" -t 3000
+elif [[ ${SCREENSHOT_MODE} == "no-clip" ]]; then
+    echo -n "${FOCUSED_MONITOR_POSITION} ${FOCUSED_MONITOR_MODE}" | grim -g - - > "${SCREENSHOT_FILENAME}"
 else
-    slurp | grim -g - - | tee >(wl-copy) > ${FILENAME_SCREENSHOT} && notify-send -i ${FILENAME_SCREENSHOT} "Screenshot of region taken" -t 1000
+    slurp | grim -g - - | tee >(wl-copy) > "${SCREENSHOT_FILENAME}" && notify-send -i "${SCREENSHOT_FILENAME}" "Screenshot of region taken" -t 3000
 fi
